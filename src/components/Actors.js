95dce1b3ed757d2bc78605de842aa212a6c9fa59
Anwar Ismail <anwar.ismail@student.moringaschool.com>
2022-09-29 12:06:00 +0300

    Completed assignment

--- src/components/Actors.js
+++ src/components/Actors.js
@@ -2,7 +2,17 @@ import React from "react";
 import { actors } from "../data";
 
 function Actors() {
-  return <div>{/*{code here}*/}</div>;
+  return (
+  <>
+    <h1>Actors Page</h1>
+    {actors.map((actor)=>(
+      <div key={actor.name}>
+        <h2>Name :{actor.name}</h2>
+        <ul>Movies:{actor.movies.map ((movie)=><li key={movie}>{movie}</li>)}</ul>
+      </div>
+    ))}
+  </>
+    );
 }
 
 export default Actors;
--- src/components/Movies.js
+++ src/components/Movies.js
@@ -2,7 +2,22 @@ import React from "react";
 import { movies } from "../data";
 
 function Movies() {
-  return <div>{/*{code here}*/}</div>;
+  
+
+
+  return (
+  <>
+    <h1>Movies Page</h1>
+    {movies.map((movie)=>(
+      <div key={movie.title}>
+        <h2>{movie.title}</h2>
+        <p>Time :{movie.time}</p>
+        <ul>Genres: {movie.genres.map((genre)=><li key={genre}>{genre}</li>)}</ul>
+      </div>
+      
+    ))}
+    </>
+    );
 }
 
 export default Movies;
